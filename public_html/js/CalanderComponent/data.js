52cc5986e7b5c1195ffe04a99c4ecd461e2a8c7a
ag1454 <98437414+ag1454@users.noreply.github.com>
2022-11-10 12:55:07 -0500

    Add files via upload

---
+++ public_html/js/CalanderComponent/data.js
@@ -0,0 +1,4 @@
+export var adminView = "Data base is yet not connected data is null";
+export var employeeView = "you are not scheduled for this day!!!";
+export var scheduleMaker; //= [[[g],[r],[r],[r],[g],[g],[r]],[[g],[g],[g],[r],[g],[g],[r]],[[r],[r],[g],[r],[r],[r],[r]]];
+
