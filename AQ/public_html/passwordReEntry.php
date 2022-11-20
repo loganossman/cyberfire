51108adeb06f773fc215948d408aff46d9f95a8b
Logan Ossman <loganossman@gmail.com>
2022-11-19 22:17:38 -0500

    Update passwordReEntry.php

--- AQ/public_html/passwordReEntry.php
+++ AQ/public_html/passwordReEntry.php
@@ -8,10 +8,15 @@ include "../private_html/config.php";
 include PRIVATE_PATH . "db.inc.php";
 
  if($_POST['passwordOne'] == $_POST['passwordTwo']){
-   $sql = "UPDATE employee SET password=" . $_POST['passwordOne'] . "WHERE email = :email;";
+   $sql = "UPDATE employee SET password= :password WHERE email = :email;";
+   $arguments = ['password'];
+   $arguments['password'] = $_POST['passwordOne'];
+   $arguments = ['email'];
+   $arguments['email'] = $_SESSION['username'];
    $stmt = $pdo->prepare($sql);
-   $stmt->execute(['email' => $_SESSION["email"]]);
+   $stmt->execute($arguments);
    $user = $stmt->fetch();
+   echo($user);
    $smarty->display("passwordChangeVerification.tpl");
  }
  else{
