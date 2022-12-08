a4a3a97eeaf282c5e1ed258d6dee9d6eb4fac528
Logan Ossman <loganossman@gmail.com>
2022-12-07 22:06:36 -0500

    Sure

--- AQ/public_html/passcode.php
+++ AQ/public_html/passcode.php
@@ -7,7 +7,7 @@ include "../private_html/config.php";
 include PRIVATE_PATH . "db.inc.php";
 
 if(!isset($_POST["passcodeEntry"])){
-    $retval = mail("loganossman@gmail.com", "Egg", "Your passcode is 185769");
+    $retval = mail("loganossman@gmail.com", "Egg", "Your passcode is 185769", "My Header");
     echo($retval);
     $smarty->display("passcode.tpl");
 }
