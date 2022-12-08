28f148dd5aa5ab2c81b297893d9dae2d1976f7b4
Logan Ossman <loganossman@gmail.com>
2022-12-07 22:43:25 -0500

    Beginning Hash Work

--- AQ/public_html/passwordReEntry.php
+++ AQ/public_html/passwordReEntry.php
@@ -8,7 +8,8 @@ include "../private_html/config.php";
 include PRIVATE_PATH . "db.inc.php";
 
  if($_POST['passwordOne'] == $_POST['passwordTwo']){
-   $sql = "UPDATE employee SET password= '" . $_POST['passwordOne'] . "' WHERE email = :email;";
+   $hashVal = hash("md5", $POST['passwordOne'], false);
+   $sql = "UPDATE employee SET password= '" . $hashVal . "' WHERE email = :email;";
    $stmt = $pdo->prepare($sql);
    $stmt->execute(['email' => $_SESSION['username']]);
    $user = $stmt->fetch();
