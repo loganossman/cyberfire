39b71d91f8f712af589a919b039b0eb12453e631
Logan Ossman <loganossman@gmail.com>
2022-12-08 00:05:45 -0500

    Update loginForm.php

--- public_html/loginForm.php
+++ public_html/loginForm.php
@@ -33,8 +33,6 @@ if (isset($_POST['submit'])) {
     } else {
         //echo '<script>alert("Incorrect email or password")</script>';
         $smarty->assign("error", "<p style=" . "color:red;" . ">Incorrect email or password!</p>");
-        echo($hash[0]);
-        echo($hashword);
         $smarty->display("login.tpl");
         echo($hash[0]);
         echo($hashword);
