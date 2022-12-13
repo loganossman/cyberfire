d7e60cf4e641e0898fe64c37fd12b25a8ccef4cc
Logan Ossman <loganossman@gmail.com>
2022-12-13 00:25:47 -0500

    Skill Problem

--- public_html/passcode.php
+++ public_html/passcode.php
@@ -8,9 +8,9 @@ include "../private_html/config.php";
 include PRIVATE_PATH . "db.inc.php";
 
 if(!isset($_POST["passcodeEntry"])){
-    mail($_SESSION["username"], "Your Reset Passcode", "Your passcode is 185769");
     $_SESSION["passcode"] = rand(100000, 999999);
     echo($_SESSION["passcode"]);
+    mail($_SESSION["username"], "Your Reset Passcode", "Your passcode is 185769");
     $smarty->display("passcode.tpl");
     echo($_SESSION["passcode"]);
 }
