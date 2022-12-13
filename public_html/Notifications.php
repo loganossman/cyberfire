9c08d1641b5f34c4ebfa3cf4db077aaf82c82ec6
Logan Ossman <loganossman@gmail.com>
2022-12-13 02:18:29 -0500

    Update Notifications.php

--- public_html/Notifications.php
+++ public_html/Notifications.php
@@ -2,8 +2,33 @@
 /**
  * @var $smarty
  */
+session_start();
 
 include "../private_html/config.php";
+
+$sql = "SELECT * FROM message WHERE sender_id = " . $_SESSION["myID"] . " OR receiver_id = " . $_SESSION["myID"] . ";";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$messageTable = $stmt->fetch();
+
+$blackList = [];
+
+foreach $messageTable in $messages{
+    if(!((in_array($messages["sender_id"], $blackList)) or (in_array($messages["receiver_id"], $blackList)))){
+        $conversation = [];
+        foreach $messageTable in $singleConvo{
+            if()
+        }
+        if($messages["sender_id"] == $_SESSION["myID"]){
+            $blackList[] = $messages["sender_id"];
+        }
+        else{
+            $blackList[] = $messages["receiver_id"];
+        }
+    }
+}
+
+
 
 $smarty->display("Notifications.tpl");
 
