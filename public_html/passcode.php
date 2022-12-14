bd48797eea38878bded9ec4ac0ec7feddf0314c5
Logan Ossman <loganossman@gmail.com>
2022-12-14 02:34:34 -0500

    Update passcode.php

--- public_html/passcode.php
+++ public_html/passcode.php
@@ -11,11 +11,12 @@ if(!isset($_POST["passcodeEntry"])){
     $smarty->display("passcode.tpl");
     echo($_SESSION["passcode"]);
 }
-elseif($_POST["passcodeEntry"] == strval($SESSION["passcode"])){
+elseif($_POST["passcodeEntry"] == strval($_SESSION["passcode"])){
     $smarty->display("passwordReEntry.tpl");
 }
 else{
     echo "this is not the code";
     $_SESSION["passcode"] = rand(100000, 999999);
     echo($_SESSION["passcode"]);
+    $smarty->display("passcode.tpl");
 }
