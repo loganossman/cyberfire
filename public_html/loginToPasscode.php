892be5acb59f4443b0169e59423f6bdc9b6a4298
Logan Ossman <loganossman@gmail.com>
2022-12-14 02:27:43 -0500

    Update loginToPasscode.php

--- public_html/loginToPasscode.php
+++ public_html/loginToPasscode.php
@@ -11,6 +11,9 @@ $stmt = $pdo->prepare($sql);
 $stmt->execute(['email' => $_POST["email"]]);
 $user = $stmt->fetch();
 if($user){
+    $_SESSION["passcode"] = rand(100000, 999999);
+    echo($_SESSION["passcode"]);
+    mail("loganossman@gmail.com", "Basic Subject", "Your passcode is X");
     $_SESSION["username"] = $_POST["email"];
     $smarty->display("passcode.tpl");
 }
