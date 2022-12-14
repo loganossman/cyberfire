0d0640d512c285a86b7790ef71cf069691c448f8
Logan Ossman <loganossman@gmail.com>
2022-12-14 02:19:43 -0500

    Update passcode.php

--- public_html/passcode.php
+++ public_html/passcode.php
@@ -10,7 +10,6 @@ include PRIVATE_PATH . "db.inc.php";
 if(!isset($_POST["passcodeEntry"])){
     $_SESSION["passcode"] = rand(100000, 999999);
     echo($_SESSION["passcode"]);
-    mail($_SESSION["username"], "Your Reset Passcode", "Your passcode is 185769");
     $smarty->display("passcode.tpl");
     echo($_SESSION["passcode"]);
 }
