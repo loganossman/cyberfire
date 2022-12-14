d44e4f0c23ac3aa35887a94ae138f11bc70710d3
Logan Ossman <loganossman@gmail.com>
2022-12-14 03:03:22 -0500

    Finished

--- public_html/Notifications.php
+++ public_html/Notifications.php
@@ -19,10 +19,7 @@ $stmtCopyTwo = $secondStmt;
 
 $blackList = [];
 $conversationContainer = [];
-echo("boom baby <br>");
-echo($sql);
 foreach ($stmtCopy as $messages){
-    echo("egg <br>");
     if(!((in_array($messages["sender_id"], $blackList)) or (in_array($messages["receiver_id"], $blackList)))){
         $conversation = [];
         $their_id = "";
@@ -32,13 +29,11 @@ foreach ($stmtCopy as $messages){
         else{
             $their_id = $messages["sender_id"];
         }
-        echo($their_id);
         foreach ($stmtCopyTwo as $singleConvo){
             if(($singleConvo["sender_id"] ==  $_SESSION["myID"] and $singleConvo["receiver_id"] == $their_id) or ($singleConvo["receiver_id"] ==  $_SESSION["myID"] and $singleConvo["sender_id"] == $their_id)){
                 $conversation[] = $singleConvo;
             }
         }
-        echo("Finished One Iteration");
         if($messages["sender_id"] == $_SESSION["myID"]){
             $blackList[] = $messages["receiver_id"];
         }
