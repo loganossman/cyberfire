0437dfae1b3da10cfd4fcb124369c8f43b92f98d
Ricardoianpadilla <98437460+Ricardoianpadilla@users.noreply.github.com>
2022-12-14 12:23:47 -0500

    update

--- public_html/libs/smarty-4.2.1/templates_c/e818446e3c955a60287ef1a224e3ca2c2a963e67_0.file.EmployeeSchedule.tpl.php
+++ public_html/libs/smarty-4.2.1/templates_c/e818446e3c955a60287ef1a224e3ca2c2a963e67_0.file.EmployeeSchedule.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.2.1, created on 2022-12-02 15:24:07
+/* Smarty version 4.2.1, created on 2022-12-14 12:06:40
   from 'C:\Apache24\htdocs\cyberfire\public_html\templates\EmployeeSchedule.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.2.1',
-  'unifunc' => 'content_638a5ee7231a99_42478470',
+  'unifunc' => 'content_639a02a00c33f6_18085223',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'e818446e3c955a60287ef1a224e3ca2c2a963e67' => 
     array (
       0 => 'C:\\Apache24\\htdocs\\cyberfire\\public_html\\templates\\EmployeeSchedule.tpl',
-      1 => 1670012617,
+      1 => 1671037595,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_638a5ee7231a99_42478470 (Smarty_Internal_Template $_smarty_tpl) {
+function content_639a02a00c33f6_18085223 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -59,18 +59,18 @@ function content_638a5ee7231a99_42478470 (Smarty_Internal_Template $_smarty_tpl)
             </div>
     
             <aside class = "aside">
-                <div class="boldJohn">Today:</div>
-                <div>Westside: 10am-8pm</div>
-                <div class="boldJohn">Tomorrow:</div>
-                <div>Westside: 10am-8pm</div>
-                <div class="boldJohn">May 8, 2022:</div>
-                <div>HRC: 10am-8pm</div>
-                <div class="boldJohn">May 9, 2022:</div>
-                <div>HRC: 10am-8pm</div>
-                <div class="boldJohn">May 10, 2022:</div>
-                <div>Westside: 10am-8pm</div>
-                <div class="boldJohn">May 11, 2022:</div>
-                <div>Westside: 10am-8pm</div>
+                <div id ="1d"class="boldJohn">Today:</div>
+                <div id ="1t">Westside: 10am-8pm</div>
+                <div id ="2d"class="boldJohn">Tomorrow:</div>
+                <div id ="2t">Westside: 10am-8pm</div>
+                <div id ="3d"boldJohn">May 8, 2022:</div>
+                <div id ="3t">HRC: 10am-8pm</div>
+                <div id ="4d"class="boldJohn">May 9, 2022:</div>
+                <div id="4t">HRC: 10am-8pm</div>
+                <div id="5d" class="boldJohn">May 10, 2022:</div>
+                <div id="5t">Westside: 10am-8pm</div>
+                <div id="6d"class="boldJohn">May 11, 2022:</div>
+                <div id="6t">Westside: 10am-8pm</div>
             </aside>
         </div>
         
@@ -86,7 +86,7 @@ function content_638a5ee7231a99_42478470 (Smarty_Internal_Template $_smarty_tpl)
           </button>
         </div>
         <div id="contain" class="modal-body">
-          ...
+          
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -110,18 +110,225 @@ function content_638a5ee7231a99_42478470 (Smarty_Internal_Template $_smarty_tpl)
  src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"><?php echo '</script'; ?>
 >
 <?php echo '<script'; ?>
-> 
-    function show(type,id,data,globalid){
-        $('#myModal').modal('show')
-        var h5 = document.querySelector(".modal-header h5");
-       h5.innerHTML = id;
-       var element = document.createElement("div");
-        element.className = "viewOfContain";
-        element.appendChild(document.createTextNode(data));
-        document.getElementById("contain").appendChild(element);
-    
+>
+    var array = <?php echo json_encode($_smarty_tpl->tpl_vars['array']->value);?>
+;
+
+function getDate(i) {
+
+    var Months = [
+                            'January',
+                            'February',
+                            'March',
+                            'April',
+                            'May',
+                            'June',
+                            'July',
+                            'August',
+                            'September',
+                            'October',
+                            'November',
+                            'December'
+];
+let add = 1 + i;
+var today = new Date();
+var dd =  today.getDate() + add;
+
+var mm =  today.getMonth() + 1; 
+var yyyy =  today.getFullYear();
+
+
+function exist(date, location){
+                        let exist = "you do not work this day"
+                        for (var i = 0, len = Object.keys(array).length; i < len; i++) {
+                        console.log("array date: "+array[i+1]['date']+"  date:"+date)
+                        if(array[i+1]['date'] == date ) {
+                            return ("You work at " + array[i+1]['location']+ " from " + array[i+1]['shift']);
+                        }   
+                        }
+                        return exist;
+                    };
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} if (i == -1){
+    today = "Today";
+    date = yyyy+"-"+mm+"-"+dd;
+    console.log(date);
+    today2 = exist(date);
+
+}else if(i == 0){
+    today = "Tommorow";
+    date = yyyy+"-"+mm+"-"+dd;
+    console.log(date);
+
+    today2 = exist(date);
+
+
+}else{
+    console.log("month num: "+ mm);
+    console.log("month nword: "+ Months[mm]);
+    console.log("day num: "+ dd);
+    console.log("year num: "+ yyyy);
+
+                    date = yyyy+"-"+mm+"-"+dd;
+                    today2 = exist(date);
+
+    today = Months[mm-1]+" "+dd+", "+yyyy;
 }
-<?php echo '</script'; ?>
+
+ console.log(i+"d")              
+document.getElementById(String((i+2)+'d')).innerHTML = String(today);
+document.getElementById(String((i+2)+'t')).innerHTML = String(today2);
+console.log("this is gen "+today);
+}
+for (var i = -1, len = 5; i < len; i++) {
+    console.log(i);
+    getDate(i);
+}
+getDate(2);
+
+    console.log("started");
+    console.log("Hello");
+    console.log(array);
+    setTimeout(() => { rerender();
+
+    }, 100);
+    function rerender(){
+        var month = (document.getElementsByTagName('h3')[1]).innerHTML; 
+        console.log(month) ;
+        for (var i = 0, len = numOfDaysInMonth(month); i < len; i++) {
+            if(exist(convert(month+" "+i+", 2022")) == true ){
+                console.log("i am runnnig");
+                let id =  month+" "+i+", 2022";
+                console.log(id);
+                document.getElementById(id).className = "full_calander__number green";
+            }
+        }
+    }
+    var day31MonthArr=[
+        "January",
+        "March",
+        "May",
+        "July",
+        "August",
+        "October",
+        "December",
+    ];
+    function convert(date){
+                    var date = date.replace(',' , '');
+                    var date = date.split(" ");
+                    var Months = {
+                            January: "1",
+                            February: "2",
+                            March: "3",
+                            April: "4",
+                            May: "5",
+                            June: "6",
+                            July: "7",
+                            August: "8",
+                            September: "9",
+                            October: "10",
+                            November: "11",
+                            December: "12"
+                    };
+                    month = Months[date[0]];
+                    day = date[1];
+                    year = date[2];
+                    date = year+"-"+month+"-"+day
+                    return date;
+    };
+    function exist(date, location){
+                        let exist = false
+                        for (var i = 0, len = Object.keys(array).length; i < len; i++) {
+                        if(array[i+1]['date'] == date ) {
+                            exist = true;
+                        }   
+                        }
+                        return exist;
+                    };
+    function numOfDaysInMonth(month){
+        let days = 0;
+        if (month == "February"){
+            days = 29
+        } else if (day31MonthArr.includes(month)){
+            days = 31;
+        } else{
+            days = 30;
+        }
+        return days;
+    };
+    let first = 0;
+        
+        function show(type,id,data){
+        
+            console.log(id);
+                    
+                    
+  
+                
+                $('#myModal').modal('show')
+                var h5 = document.querySelector(".modal-header h5");
+                val = id;
+                h5.innerHTML = id ; 
+                let view = [];
+                for (var i = 0, len = Object.keys(array).length; i < len; i++) {
+                    console.log(convert(id)+" == " + array[i+1]['date']);
+
+                    if (convert(id) == array[i+1]['date']){
+                        
+                        view.push(array[i+1]);
+                        console.log("pushing");
+                        console.log(array[i+1]);
+                    };
+
+                };
+                console.log(view);
+
+                if(first == 1){
+                    document.getElementById("v").remove();
+                    document.getElementById("vv").remove();
+                }
+                var element2 = document.createElement("B");
+                element2.className = "viewOfContain", element2.id= "vv";
+                
+                
+            
+                element2.innerHTML = "Schedule:";
+                document.getElementById("contain").appendChild(element2);
+                var element = document.createElement("div");
+            
+
+            element.className = "viewOfContain", element.id= "v";
+            console.log("This is view: "+view[0]);
+            if(view[0] != null){
+              for (var i = 0, len = 1; i < len; i++) {
+                    element.appendChild(document.createTextNode(("You are working at "+(view[i]['location']) +" from: "+ (view[i]['shift']))));
+                    element.appendChild(document.createElement("br"));
+                };
+            }
+                
+                console.log(view[1]);
+                if(view[0] == null){
+                    console.log("hi");
+                    element2.innerHTML = "No schuedule has been set for this day";
+
+                }
+
+                document.getElementById("contain").appendChild(element);
+                first = 1;  
+        }
+        
+        
+        function closer(){
+          console.log("clear is running");
+        }
+    <?php echo '</script'; ?>
 > 
 </body>
 </html><?php }
--- public_html/loginForm.php
+++ public_html/loginForm.php
@@ -13,7 +13,15 @@ include PRIVATE_PATH . "db.inc.php";
 if (isset($_POST['submit'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
+
+    $sql3 = "SELECT user_id FROM employee WHERE email='" . $email . "'";
+    $stmt3 = $pdo->prepare($sql3);
+    $stmt3->execute();
+    $user3 = $stmt3->fetch();
+    $_SESSION["id"] = $user3;
+
     $_SESSION["email"] = $email;
+
 
     // check if email matches an email in Employee table
     $sql = "SELECT username FROM employee WHERE email='" . $email . "'";
@@ -27,7 +35,7 @@ if (isset($_POST['submit'])) {
     $pass = $stmt2->fetch();
     
     if ($pass and $user) {
-        $smarty->display("EmployeeSchedule.tpl");
+        header("Location: ../public_html/EmployeeSchedule.php");
     } else {
         //echo '<script>alert("Incorrect email or password")</script>';
         $smarty->assign("error", "<p style=" . "color:red;" . ">Incorrect email or password!</p>");
